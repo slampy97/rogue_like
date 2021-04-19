47e57d59e5238d06ac7db80f73d8de6b8d88f131
slampy97 <slampy97@gmail.com>
2021-04-19 10:38:37 +0300

    init commit

---
+++ src/Actor.java
@@ -0,0 +1,10 @@
+public class Actor {
+  private int coordx;
+  private int coordy;
+
+  Actor(int x, int y) {
+    coordx = x;
+    coordy = y;
+  }
+}
+
---
+++ src/RogueLike.java
@@ -0,0 +1,5 @@
+public class RogueLike {
+  public static void main(String[] args) {
+
+  }
+}
